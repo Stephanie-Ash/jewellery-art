c0057c0420bcc03536d337e2bb13bb1d3254b861
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-01-11 11:11:08 +0000

    Added home app and template

---
+++ home/views.py
@@ -0,0 +1,10 @@
+""" Views for the home app. """
+from django.shortcuts import render
+
+
+def index(request):
+    """
+    A view to return the homepage.
+    """
+
+    return render(request, 'home/index.html')
