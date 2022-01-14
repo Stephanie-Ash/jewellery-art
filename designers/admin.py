5743e6469272d2ce0602a80eae49bdeb0e0c29f4
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-01-14 15:50:39 +0000

    Register the Designer, Collection, Category and Product models on the admin.

--- designers/admin.py
+++ designers/admin.py
@@ -1,3 +1,21 @@
+""" Admin panel set-up for the designers app. """
 from django.contrib import admin
+from .models import Designer, Collection
 
-# Register your models here.
+
+@admin.register(Designer)
+class DesignerAdmin(admin.ModelAdmin):
+    """
+    Admin options for the Designer model.
+    """
+    list_display = ('name', 'phone_number', 'email', 'image')
+    ordering = ('name',)
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    """
+    Admin options for the Collection model.
+    """
+    list_display = ('name', 'programmatic_name', 'designer')
+    ordering = ('name',)
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
+""" Admin panel set-up for the products app. """
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin options for the Category model.
+    """
+    list_display = ('name', 'programmatic_name')
+    ordering = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Admin options for the Product model.
+    """
+    list_display = (
+        'sku', 'name', 'category', 'collection', 'designer', 'price', 'image')
+    ordering = ('sku',)
