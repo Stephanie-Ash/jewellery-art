e958c98f837b1ee3df7881985b831bb06c0fed8d
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-01-19 13:25:58 +0000

    Add collection filtering to all_product view and set nav links in base.html

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 """ Views for the products app. """
 from django.shortcuts import render
 
-from designers.models import Designer
+from designers.models import Designer, Collection
 from .models import Product, Category
 
 
@@ -13,6 +13,7 @@ def all_products(request):
     products = Product.objects.all()
     category = None
     designer = None
+    collection = None
 
     if request.GET:
         if 'category' in request.GET:
@@ -25,10 +26,18 @@ def all_products(request):
             products = products.filter(designer__programmatic_name=designer)
             designer = Designer.objects.filter(programmatic_name=designer)
 
+        if 'collection' in request.GET:
+            collection = request.GET['collection']
+            products = products.filter(
+                collection__programmatic_name=collection)
+            collection = Collection.objects.filter(
+                programmatic_name=collection)
+
     context = {
         'products': products,
         'current_category': category,
         'current_designer': designer,
+        'current_collection': collection,
     }
 
     return render(request, 'products/products.html', context)
