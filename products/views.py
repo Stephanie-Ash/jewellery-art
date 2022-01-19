e4edcceb666670e01aafcb32b6033bed289377ec
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-01-19 10:37:51 +0000

    Add category filtering to all_products view and add urls to navigation in base.html

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 """ Views for the products app. """
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
@@ -9,9 +9,17 @@ def all_products(request):
     This will include filtering, sorting and searching.
     """
     products = Product.objects.all()
+    category = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            category = request.GET['category']
+            products = products.filter(category__programmatic_name=category)
+            category = Category.objects.filter(programmatic_name=category)
 
     context = {
         'products': products,
+        'current_category': category
     }
 
     return render(request, 'products/products.html', context)
