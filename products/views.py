853739ffaa5511fcdecc016c9f8728f5a11cfc67
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-01-19 14:14:57 +0000

    Add sorting to all products view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 """ Views for the products app. """
 from django.shortcuts import render
+from django.db.models.functions import Lower
 
 from designers.models import Designer, Collection
 from .models import Product, Category
@@ -14,8 +15,28 @@ def all_products(request):
     category = None
     designer = None
     collection = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if sortkey == 'designer':
+                sortkey = 'designer__name'
+            if sortkey == 'collection':
+                sortkey = 'collection__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             category = request.GET['category']
             products = products.filter(category__programmatic_name=category)
@@ -33,11 +54,14 @@ def all_products(request):
             collection = Collection.objects.filter(
                 programmatic_name=collection)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'current_category': category,
         'current_designer': designer,
         'current_collection': collection,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
