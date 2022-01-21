96e8c77364d17539894dc0bb15546b91d3a3b01d
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-01-21 09:04:17 +0000

    Add homepage_featured field to product model

--- products/models.py
+++ products/models.py
@@ -51,6 +51,7 @@ class Product(models.Model):
     material = models.CharField(max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
+    homepage_featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
