e46e663ca56ed792838306ab4b8acb94cdab81ef
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-01-21 10:26:02 +0000

    Add top picks section to homepage using homepage_featured field

--- home/views.py
+++ home/views.py
@@ -1,10 +1,16 @@
 """ Views for the home app. """
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """
     A view to return the homepage.
     """
+    featured_products = Product.objects.filter(homepage_featured=True)
 
-    return render(request, 'home/index.html')
+    context = {
+        'featured_products': featured_products,
+    }
+
+    return render(request, 'home/index.html', context)
