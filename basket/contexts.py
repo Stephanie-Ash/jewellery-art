8861fb31e13025e5a7750ce3fe07150829eb0d18
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-01-27 11:26:24 +0000

    Add content to shopping basket page

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,8 @@
 """ Context processor for the basket app. """
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+
+from products.models import Product
 
 
 def basket_contents(request):
@@ -11,6 +14,18 @@ def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     delivery = settings.STANDARD_DELIVERY
     grand_total = delivery + total
 
