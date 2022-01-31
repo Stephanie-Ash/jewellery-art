70b9a5838879257ffc398c2c64690e4000920e36
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-01-31 09:56:02 +0000

    Add signals to the checkout app to update the order total when lineitems added

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,35 @@
+""" Admin panel set-up for the checkout app. """
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    Admin inline display of order lineitems.
+    """
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Admin options for the Order model.
+    """
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'address1', 'address2',
+              'town_or_city', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
