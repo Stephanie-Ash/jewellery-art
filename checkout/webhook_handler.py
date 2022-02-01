35f272df0e58afdf6e9e47b8a21f6304c199b60b
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-01 15:12:40 +0000

    Add webhook handler and url to checkout app

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,34 @@
+""" Checkout webhook handler. """
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+    """
+    Webhook handler to handle Stripe webhooks.
+    """
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle generic/unknown/unexpected webhook event.
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle payment_intent.succeeded webhook from Sripe.
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle payment_intent.payment_failed webhook from Sripe.
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
