981d09176e1a43cf4bbc6c428677d4bd0c85c216
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-01 09:02:46 +0000

    Add initial checkout functionality to stripe_elements.js and checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
 """ Views for the checkout app. """
+import stripe
+
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
+from basket.contexts import basket_contents
 from .forms import OrderForm
 
 
@@ -9,17 +13,35 @@ def checkout(request):
     """
     Display order form and checkout.
     """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "Your basket is currently empty.")
         return redirect(reverse('products'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(
+            request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51JZsjlCxBh1rPxRGbJdW3lmqVtBNfUmc0E3VuvJkTXfCZkWNO2dRYqTYoUgtJfnpidF0z9mW83881YiP51cA3vfS00xXW9TrkJ',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
--- jewellery_art/settings.py
+++ jewellery_art/settings.py
@@ -154,4 +154,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Stripe
 STANDARD_DELIVERY = 5
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
