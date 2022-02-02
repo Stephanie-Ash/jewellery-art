7442fc1fcabc6776741c4bb80d0442e9f5550b39
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-02 09:08:47 +0000

    Add billing details to stripe js and add cache_checkout view and url

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,10 @@
 """ Views for the checkout app. """
+import json
 import stripe
 
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse)
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -9,6 +12,27 @@ from basket.contexts import basket_contents
 from products.models import Product
 from .models import OrderLineItem, Order
 from .forms import OrderForm
+
+
+@require_POST
+def cache_checkout_data(request):
+    """
+    Cache checkout data to allow info to be saved when creating
+    and order from the webhook.
+    """
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'basket': json.dumps(request.session.get('basket', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment could not be processed. \
+            Please try again.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
