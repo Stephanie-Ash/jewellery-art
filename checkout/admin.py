db40157854ca5077e90c13d42499cf9410eb8c40
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-02 10:45:45 +0000

    Add original_bag and sripe_pid fields to Order model

--- checkout/admin.py
+++ checkout/admin.py
@@ -20,13 +20,15 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'address1', 'address2',
               'town_or_city', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
