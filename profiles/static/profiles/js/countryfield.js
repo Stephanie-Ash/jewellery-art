e794c358b404f9ec83f75664db713e98005e03ed
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-08 14:27:19 +0000

    Add UserProfileForm to profile page and add post handler in the view

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,12 @@
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#6d7a82');
+}
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#6d7a82');
+    } else {
+        $(this).css('color', '#242d33');
+    }
+});
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 """ Views for the profiles app. """
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -11,13 +12,24 @@ def profile(request):
     """
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    form = UserProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile successfully updated.')
+        else:
+            messages.error(
+                request, 'Profile update failed. Please check the form.')
+    else:
+        form = UserProfileForm(instance=profile)
+
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
