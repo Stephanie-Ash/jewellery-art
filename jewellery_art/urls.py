15e0a80deae37f347a5475876ea4118568ae4a93
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-08 09:01:23 +0000

    Install and configure allauth

--- jewellery_art/urls.py
+++ jewellery_art/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('accounts/', include('allauth.urls')),
     path('products/', include('products.urls')),
     path('designers/', include('designers.urls')),
     path('basket/', include('basket.urls')),
