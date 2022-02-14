b4cb921804a279b62dd5f3ca682e91e022fc7c52
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-14 13:29:51 +0000

    Add POST method to contact view and add ContactMessage to admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
+""" Admin panel set-up for the contact app. """
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    """
+    Admin options for the ContactMessage model.
+    """
+    list_display = ('date_created', 'topic', 'email', 'message')
+    ordering = ('-date_created',)
--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 """ Views for the contact app. """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .forms import ContactForm
 
@@ -8,7 +9,20 @@ def contact(request):
     """
     A view to return the contact page.
     """
-    contact_form = ContactForm()
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.success(
+                request, 'Thank you for your message. We will get back to you \
+                    as soon as we can.')
+            return redirect('home')
+        else:
+            messages.error(
+                request, 'It has not been possible to submit your message. \
+                    Please check the form.')
+    else:
+        contact_form = ContactForm()
 
     context = {
         'contact_form': contact_form,
