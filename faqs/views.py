30d636a882c5a57b9dc0ac6ed2f430a714f2f49f
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-15 15:45:01 +0000

    Add content to faqs page

--- faqs/views.py
+++ faqs/views.py
@@ -1,10 +1,19 @@
 """ Views for the faqs app. """
 from django.shortcuts import render
 
+from .models import FAQ
+
 
 def faqs(request):
     """
     A view to return the frequently asked questions page.
     """
+    questions = FAQ.objects.all()
+    categories = questions.values_list('category', flat=True)
 
-    return render(request, 'faqs/faqs.html')
+    context = {
+        'faqs': questions,
+        'categories': categories,
+    }
+
+    return render(request, 'faqs/faqs.html', context)
