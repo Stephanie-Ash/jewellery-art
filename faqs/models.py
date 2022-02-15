2b9abceea6f1ebb519ba5a44f6c01795c6f40618
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-15 13:36:35 +0000

    Add category field to FAQ model

--- faqs/models.py
+++ faqs/models.py
@@ -7,6 +7,23 @@ class FAQ(models.Model):
     Frequently asked question model to provide
     information to customers.
     """
+    ORDER = 'OR'
+    DELIVERY = 'DE'
+    PRODUCT = 'PR'
+    DESIGNER = 'DS'
+    ACCOUNT = 'WB'
+    OTHER = 'OT'
+    CATEGORY_CHOICES = [
+        ('', 'Select Category *'),
+        (ORDER, 'Orders'),
+        (DELIVERY, 'Deliveries'),
+        (PRODUCT, 'Products'),
+        (DESIGNER, 'Designers'),
+        (ACCOUNT, 'Customer Accounts'),
+        (OTHER, 'Other'),
+    ]
+
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
     question = models.CharField(max_length=250)
     answer = models.TextField()
 
