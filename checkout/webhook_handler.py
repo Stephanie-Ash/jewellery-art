b0eb6f05b07232dca749060eab305e6551131489
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-18 12:36:40 +0000

    Reenable country in webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -65,7 +65,7 @@ class StripeWH_Handler:
                     full_name__iexact=shipping_details.name,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
-                    # country__iexact=shipping_details.address.country,
+                    country__iexact=shipping_details.address.country,
                     postcode__iexact=shipping_details.address.postal_code,
                     town_or_city__iexact=shipping_details.address.city,
                     address1__iexact=shipping_details.address.line1,
