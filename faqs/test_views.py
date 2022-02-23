945795d551e2f00101b722729064915895f72400
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-23 13:29:37 +0000

    Add test for the get add_faq  page view

--- faqs/test_views.py
+++ faqs/test_views.py
@@ -1,17 +1,30 @@
 """ Testcases for the faqs app views. """
 from django.test import TestCase
+from django.contrib.auth.models import User
 from .models import FAQ
 
 
 class TestViews(TestCase):
     """ Tests for the views"""
+    def setUp(self):
+        self.superuser = User.objects.create_superuser(
+            'admin', 'admin@email.com', 'adminpassword'
+        )
 
-    def test_get_index_page(self):
-        """ Test the faqs page loads. """
-        FAQ.objects.create(
+        self.faq = FAQ.objects.create(
             category='OR', question='Test question',
             answer='Test answer'
         )
+
+    def test_get_faq_page(self):
+        """ Test the faqs page loads. """
         response = self.client.get('/faqs/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'faqs/faqs.html')
+
+    def test_get_add_faq_page(self):
+        """ Test the add faq page loads. """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get('/faqs/add/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'faqs/add_faq.html')
