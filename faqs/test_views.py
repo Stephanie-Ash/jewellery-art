0e873518859f72c7f51984039cc33d09be1c0896
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-23 13:34:36 +0000

    Add test for the get edit_faq page view

--- faqs/test_views.py
+++ faqs/test_views.py
@@ -28,3 +28,10 @@ class TestViews(TestCase):
         response = self.client.get('/faqs/add/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'faqs/add_faq.html')
+
+    def test_get_edit_faq_page(self):
+        """ Test the edit faq page loads. """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get(f'/faqs/edit/{self.faq.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'faqs/edit_faq.html')
