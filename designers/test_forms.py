2709c187ab284a7eacd592f120153480032b0d0e
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-24 10:29:06 +0000

    Test required model fields are required on Designer form

---
+++ designers/test_forms.py
@@ -0,0 +1,19 @@
+""" Testcases for the designer app forms. """
+from django.test import TestCase
+from .forms import DesignerForm
+
+
+class TestForms(TestCase):
+    """ Tests for the forms. """
+    def test_ensure_fields_are_required(self):
+        """
+        Test that the Designer name and introduction fields are
+        required on the form.
+        """
+        form = DesignerForm({'name': '', 'introduction': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertIn('introduction', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+        self.assertEqual(
+            form.errors['introduction'][0], 'This field is required.')
