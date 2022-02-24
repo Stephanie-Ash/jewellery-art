3b6954011fd608426082e03e10900d06eff7748a
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-24 11:03:07 +0000

    Test the add and edit product pages load

--- products/test_views.py
+++ products/test_views.py
@@ -30,3 +30,17 @@ class TestViews(TestCase):
         response = self.client.get(f'/products/{self.product.id}/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/product_detail.html')
+
+    def test_get_add_product_page(self):
+        """ Test the add product page loads. """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get('/products/add/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/add_product.html')
+
+    def test_get_edit_product_page(self):
+        """ Test the edit product page loads. """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get(f'/products/edit/{self.product.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/edit_product.html')
