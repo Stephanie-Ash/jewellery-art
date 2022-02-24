b4eca0a869fd163fb9eb27d0d852c1a8733dd0f9
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-24 08:22:29 +0000

    Test the ContactMessage string method

---
+++ contact/test_models.py
@@ -0,0 +1,13 @@
+""" Testcases for the contact app models. """
+from django.test import TestCase
+from .models import ContactMessage
+
+
+class TestModels(TestCase):
+    """ Tests for the models. """
+    def test_contact_message_string_method_includes_names(self):
+        """ Test the ContactMessage model string method. """
+        contact_msg = ContactMessage.objects.create(
+            topic='OR', first_name='John', last_name='Doe',
+            email='email@email.com', message='Test message.')
+        self.assertEqual(str(contact_msg), 'Message from John Doe')
