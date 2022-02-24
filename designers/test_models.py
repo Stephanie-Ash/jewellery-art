31841f6fe61abb3744e94d94563983f93fce0595
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-24 10:26:10 +0000

    Test that the Designer and Collection programmatic_names are generated on save

--- designers/test_models.py
+++ designers/test_models.py
@@ -20,3 +20,19 @@ class TestModels(TestCase):
     def test_collection_string_method_returns_name(self):
         """ Test the Collection model string method. """
         self.assertEqual(str(self.collection), 'Test Collection')
+
+    def test_designer_programmatic_name_generated(self):
+        """
+        Test that a programmatic name for Designer model objects
+        is generated on save and that it is the lowercase
+        Designer name separated by _.
+        """
+        self.assertEqual(self.designer.programmatic_name, 'jane_doe')
+
+    def test_collection_programmatic_name_generated(self):
+        """
+        Test that a programmatic name for Collection model objects
+        is generated on save and that it is the lowercase
+        Collection name separated by _.
+        """
+        self.assertEqual(self.collection.programmatic_name, 'test_collection')
