3b580eaab30a4a794694cf527d875d19cf2a6ea4
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-24 09:11:18 +0000

    Test the add designer and edit designer pages load

--- designers/test_views.py
+++ designers/test_views.py
@@ -30,3 +30,17 @@ class TestViews(TestCase):
         response = self.client.get(f'/designers/{self.designer.id}/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'designers/designer_detail.html')
+
+    def test_get_add_desiger_page(self):
+        """ Test the add designer page loads. """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get('/designers/add/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'designers/add_designer.html')
+
+    def test_get_edit_designer_page(self):
+        """ Test the edit designer page loads. """
+        self.client.login(username='admin', password='adminpassword')
+        response = self.client.get(f'/designers/edit/{self.designer.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'designers/edit_designer.html')
