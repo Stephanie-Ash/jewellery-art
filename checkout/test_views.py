abe7f389c53b9a8373594a6a60e66fc1544b5731
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-02-28 11:04:47 +0000

    Test checkout page loads

--- checkout/test_views.py
+++ checkout/test_views.py
@@ -1,5 +1,33 @@
 """ Testcases for the checkout app views. """
 from django.test import TestCase
 from django.contrib.auth.models import User
+from products.models import Product
 
 
+class TestViews(TestCase):
+    """ Tests for the views. """
+    def setUp(self):
+        self.user = User.objects.create_user(
+            'john', 'john@email.com', 'johnpassword'
+        )
+
+        self.product_one = Product.objects.create(
+            name='Test Product One', description='Test description.',
+            price=50.00, inventory=0
+        )
+
+        self.product_two = Product.objects.create(
+            name='Test Product Two', description='Test description.',
+            price=20.00, inventory=2
+        )
+
+    def test_get_checkout_page(self):
+        """ Test the checkout page loads. """
+        session = self.client.session
+        session['basket'] = {
+            self.product_two.id: 1,
+        }
+        session.save()
+        response = self.client.get('/checkout/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkout/checkout.html')
