a383df9e8732b13519480f44ee38447c8f6ae2cb
Stephanie-Ash <stephanie.ashdown@hotmail.co.uk>
2022-03-02 11:58:22 +0000

    Add comments and adjust html alignment where code snippets on basket and chekout pages

--- checkout/views.py
+++ checkout/views.py
@@ -233,6 +233,7 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+        'country_code': country_code,
     }
 
     return render(request, template, context)
